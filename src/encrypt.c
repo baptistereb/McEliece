68ee74489ff1957f978238a0b300b14f11db4542
Baptiste <baptiste.rebillard@outlook.fr>
2024-01-29 12:01:47 +0100

    correction

--- src/encrypt.c
+++ src/encrypt.c
@@ -1,11 +1,12 @@
 #include "matrix.h"
+#include <stdlib.h>
 
 char ** encrypt(int size, char msg[size], int public_size, int public[size][size]) {
-	int result[size];
+	int * result;
 	int resultsize = sizeof(int)*size*16; //car 16 bit/caractères
 	result = malloc(resultsize);
 
-	multMatrix(char,resultsize,resultsize,)
+	//multMatrix(char,resultsize,resultsize,);
 	
 
 	return result;
